0af2e41eb31bfb519b3a8185ffc968d9107e3cce
MZandtheRaspberryPi <ziegltrum@gmail.com>
2024-06-16 23:48:13 +0100

    new updates

--- pi_firmware/README.md
+++ pi_firmware/README.md
@@ -1,1 +1,8 @@
 docker build -t bittle:2024-6-15 -f .\Dockerfile --progress=plain .
+
+# setting up the bittle
+on the Nyboard, on the side of the board facing the robot there is a switch. This switch should be toward the Bittle's head to be raspberry pi as i2cm aster.
+
+i2cdetect -y 1
+
+sudo docker run -it --device /dev/i2c-1 mzandtheraspberrypi/bittle:2024-6-15
--- pi_firmware/main.cpp
+++ pi_firmware/main.cpp
@@ -4,10 +4,33 @@
 #include <MPU6050.hpp>
 
 int main() {
-  std::string i2c_name = "/dev/i2c-3";
+  std::string i2c_name = "/dev/i2c-1";
   uint8_t address = MPU6050_DEFAULT_ADDRESS;
   I2CBusRaspberryPi i2c_dev(i2c_name);
+  i2c_dev.begin();
+
   MPU6050 accelgyro(address, &i2c_dev);
-  // to do, test connection
+  if ( accelgyro.testConnection() ) 
+    printf("MPU6050 connection test successful\n") ;
+  else {
+    fprintf( stderr, "MPU6050 connection test failed! something maybe wrong ...\n");
+    return 1;
+  }
+
   accelgyro.initialize();
+  
+  int16_t ax, ay, az, gx, gy, gz;
+  ax = ay = az = gx = gy = gz = 0;
+  const uintmax_t sample_num = 5000;
+  const uintmax_t samples_per_second = 1000;
+  const int64_t sleep_ms = 1000 / (int64_t)samples_per_second;
+  for (uintmax_t i = 0; i < sample_num; i++) {
+    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+    if (i % 500 == 0) {
+        std::cout << "ax: " << ax << " ay: " << ay << " az: " << az << std::endl;
+    }
+    delay(sleep_ms);
+  }
+  i2c_dev.close();
+
 }
