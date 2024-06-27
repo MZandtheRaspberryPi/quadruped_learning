b87dab50b74d020409680cd7b0e2cdcece328f48
MZandtheRaspberryPi <ziegltrum@gmail.com>
2024-06-27 21:30:38 +0100

    adding some profiling

--- pi_firmware/main.cpp
+++ pi_firmware/main.cpp
@@ -31,18 +31,37 @@ int main() {
   uintmax_t sample_num = 5000;
   const uintmax_t samples_per_second = 200;
   const int64_t sleep_ms = 1000 / (int64_t)samples_per_second;
-  servo_controller.set_servo_angle(0, 0);
+  uint64_t sensor_start, sensor_end, servo_start, servo_end, total_sensor, total_servo;
+  servo_start = millis();
+  for (uintmax_t i = 0; i < num_servos; i++)
+  {
+      servo_controller.set_servo_angle(i, 0);
+  }
+  servo_end = millis();
+  total_servo += servo_end - servo_start;
+
   for (uintmax_t i = 0; i < sample_num; i++) {
+    sensor_start = millis();
     accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+    sensor_end = millis();
+    total_sensor += sensor_end - sensor_start;
     if (i % 500 == 0) {
         std::cout << "ax: " << ax << " ay: " << ay << " az: " << az << std::endl;
     }
     if (i == sample_num / 2)
     {
-          servo_controller.set_servo_angle(0, M_PI/8);
+      servo_start = millis();
+      for (uintmax_t i = 0; i < num_servos; i++)
+      {
+          servo_controller.set_servo_angle(i, M_PI/8);
+      }
+      servo_end = millis();
+      total_servo += servo_end - servo_start;
     }
     delay(sleep_ms);
   }
   i2c_dev.close();
+  std::cout << " sensor reads took " << 1.0 * total_sensor / sample_num << " ms on average" << std::endl;
+  std::cout << " servo commands took " << 1.0 * total_servo / 2 << " ms on average" << std::endl;
 
 }
