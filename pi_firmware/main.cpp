86107232d6a193473ef0bbb491c47e5081c40e69
MZandtheRaspberryPi <ziegltrum@gmail.com>
2024-06-27 21:04:53 +0100

    testing new servo driver

--- pi_firmware/main.cpp
+++ pi_firmware/main.cpp
@@ -3,17 +3,20 @@
 #include <i2c_linux.hpp>
 #include <Adafruit_PWMServoDriver.h>
 #include <MPU6050.hpp>
+#include <servo_controller.hpp>
 #include <utils.hpp>
 
 int main() {
   std::string i2c_name = "/dev/i2c-1";
-  uint8_t address = MPU6050_DEFAULT_ADDRESS;
+  uint8_t mpu_address = MPU6050_DEFAULT_ADDRESS;
   I2CLinuxAPI i2c_dev(i2c_name);
   i2c_dev.begin();
-
-  Adafruit_PWMServoDriver motor_driver(PCA9685_I2C_ADDRESS, &i2c_dev);
-  motor_driver.begin();
-  MPU6050 accelgyro(address, &i2c_dev);
+  uint8_t num_servos = 8;
+  ServoBoardConfig servo_config = ServoBoardConfig(num_servos);
+  Adafruit_PWMServoDriver_Wrapper motor_driver(PCA9685_I2C_ADDRESS, &i2c_dev);
+  ServoController servo_controller = ServoController(&servo_config,
+                                 &motor_driver);
+  MPU6050 accelgyro(mpu_address, &i2c_dev);
   if ( accelgyro.testConnection() )
     printf("MPU6050 connection test successful\n") ;
   else {
@@ -25,11 +28,10 @@ int main() {
 
   int16_t ax, ay, az, gx, gy, gz;
   ax = ay = az = gx = gy = gz = 0;
-  const uintmax_t sample_num = 5000;
-  const uintmax_t samples_per_second = 1000;
+  uintmax_t sample_num = 5000;
+  const uintmax_t samples_per_second = 200;
   const int64_t sleep_ms = 1000 / (int64_t)samples_per_second;
-  const uint8_t pin_num = 0;
-  motor_driver.writeMicroseconds(pin_num, 700);
+  servo_controller.set_servo_angle(0, 0);
   for (uintmax_t i = 0; i < sample_num; i++) {
     accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
     if (i % 500 == 0) {
@@ -37,7 +39,7 @@ int main() {
     }
     if (i == sample_num / 2)
     {
-        motor_driver.writeMicroseconds(pin_num, 1400);
+          servo_controller.set_servo_angle(0, M_PI/8);
     }
     delay(sleep_ms);
   }
