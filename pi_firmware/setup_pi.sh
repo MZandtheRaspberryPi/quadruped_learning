7ec126e040b1642acd721fa0c7495f18f642eb6f
mzandtheraspberrypi <mzandtheraspberrypi@gmail.com>
2024-08-09 17:11:30 +0000

    updating setup script

--- pi_firmware/setup_pi.sh
+++ pi_firmware/setup_pi.sh
@@ -21,5 +21,4 @@ sudo apt-get update
 
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo docker run hello-world
-sudo groupadd docker
 sudo usermod -aG docker $USER
